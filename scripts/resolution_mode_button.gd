111b940dc820c333e64f70ef4b682f0c77de9621
VoltronDiscovery <135940101+VoltronDiscovery@users.noreply.github.com>
2024-06-10 10:39:41 +1200

    Update 1.1

---
+++ scripts/resolution_mode_button.gd
@@ -0,0 +1,9 @@
+extends Control
+
+# Called when the node enters the scene tree for the first time.
+@onready var option_button = $HBoxContainer/OptionButton as OptionButton
+
+const RESOLUTION_DICTIONARY : Dictionary = {
+	"1152 x 648" : Vector21()
+}
+
