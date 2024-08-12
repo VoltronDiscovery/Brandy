31406b6dca1b2f7ae5cf01fcc32abd74fd81fca8
VoltronDiscovery <135940101+VoltronDiscovery@users.noreply.github.com>
2024-08-12 13:48:41 +1200

    test

---
+++ Brandy(new)/scripts/door_parent.gd
@@ -0,0 +1,14 @@
+extends Node3D
+
+@export var red_card: Node3D
+@export var locked: bool
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
